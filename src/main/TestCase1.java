22b0f9754e5e6bbcb60353a08b4d50ad86fec546
ugurinanmaz <ugurinanmaz@gmail.com>
2022-07-07 20:30:38 -0500

    New Commit to see if it works

--- src/main/TestCase1.java
+++ src/main/TestCase1.java
@@ -2,6 +2,10 @@ package main;
 
 public class TestCase1 {
 
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+    }
+
     void test1(){
         System.out.println("This is test 1...");
     }
